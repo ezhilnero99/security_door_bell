5acb6fc3ffecfd10aee527287790753180d40f9a
ezhilnero <ezhil99mutsun@gmail.com>
2021-02-22 14:46:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/securitydoorbell/DomainChange.java
@@ -0,0 +1,14 @@
+package com.example.securitydoorbell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DomainChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_domain_change);
+    }
+}
